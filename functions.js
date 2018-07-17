f581b56fbd3876e23fb3b36ea5bd31d9ee8dec2e
Alexey Kozyatinskiy <kozyatinskiy@chromium.org>
2018-07-17 08:17:57 -0700

    function scopes and location example

--- functions.js
+++ functions.js
@@ -14,8 +14,52 @@ const protocol = new Proxy({}, {
   })
 });
 
-(async function main() {
-console.log(await protocol.Runtime.evaluate({expression: 'test'}));
-})()
+const c = 3;
+module.exports = {
+  test: function () {
+    var a = 1;
+    let b = 2;
+    return a + b + c;
+  }
+};
 
-function test() {}
+(async function main() {
+  // Runtime.evaluate runs expression in global context so to get variable
+  // from module this variable should be stored to global object or exported
+  // from module.
+  const {result: {objectId}} = await protocol.Runtime.evaluate({
+    expression: `require('./functions.js').test`,
+    // this flag is important, in Node.js environment it injects 'require'
+    // method in addition to different command line APIs.
+    includeCommandLineAPI: true
+  });
+  const {internalProperties} = await protocol.Runtime.getProperties({
+    objectId, ownProperties: true
+  });
+  // console.log(internalProperties);
+  const {value: {value: location}} = internalProperties.find(prop =>
+      prop.name === '[[FunctionLocation]]');
+  // returns scriptId, lineNumber and columnNumber
+  console.log(location);
+
+  const {value: {objectId: scopesObjectId}} = internalProperties.find(prop =>
+      prop.name === '[[Scopes]]');
+  // each scope returned as remote object
+  const {result: scopes} = await protocol.Runtime.getProperties({
+    objectId: scopesObjectId
+  });
+  // so we need to call getProperties ones again
+  const unwrappedScopes = await Promise.all(scopes.map(async scope => {
+    const {result: variables} = await protocol.Runtime.getProperties({
+      objectId: scope.value.objectId
+    });
+    return {variables, description: scope.value.description};
+  }));
+  for (const scope of unwrappedScopes) {
+    console.log(`--- Scope ${scope.description}`);
+    for (const variable of scope.variables) {
+      console.log(`${variable.name} = ${variable.value ? variable.value.description : 'getter...'}`);
+    }
+    console.log('---');
+  }
+})()
